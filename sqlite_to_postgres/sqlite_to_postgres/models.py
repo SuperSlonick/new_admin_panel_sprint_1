0e9ccb668d0b75ddf65c52b2ead8ed654aee7b7e
SuperSlonick <golyshkin.dima@gmail.com>
2025-03-30 14:21:43 +0300

    добавлен миксин для полей с датой создания

--- sqlite_to_postgres/sqlite_to_postgres/models.py
+++ sqlite_to_postgres/sqlite_to_postgres/models.py
@@ -2,18 +2,19 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
-
+class TimestampMixin:
+    created_at: datetime = field(default_factory=datetime.now)
+    
 @dataclass
-class Genre:
+class Genre(TimestampMixin):
     id: str
     name: str
     description: Optional[str]
-    created_at: datetime
     updated_at: datetime
 
 
 @dataclass
-class FilmWork:
+class FilmWork(TimestampMixin):
     id: str
     title: str
     description: Optional[str]
@@ -21,30 +22,26 @@ class FilmWork:
     file_path: Optional[str]
     rating: Optional[float]
     type: str
-    created_at: datetime
     updated_at: datetime
 
 
 @dataclass
-class Person:
+class Person(TimestampMixin):
     id: str
     full_name: str
-    created_at: datetime
     updated_at: datetime
 
 
 @dataclass
-class GenreFilmWork:
+class GenreFilmWork(TimestampMixin):
     id: str
     film_work_id: str
     genre_id: str
-    created_at: datetime
 
 
 @dataclass
-class PersonFilmWork:
+class PersonFilmWork(TimestampMixin):
     id: str
     film_work_id: str
     person_id: str
     role: str
-    created_at: datetime
