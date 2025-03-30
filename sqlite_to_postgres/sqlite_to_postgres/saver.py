6850735edd000dee8481ae0b79562cf3439766c4
SuperSlonick <golyshkin.dima@gmail.com>
2025-03-30 14:18:41 +0300

    добавлена батчевая логика

--- sqlite_to_postgres/sqlite_to_postgres/saver.py
+++ sqlite_to_postgres/sqlite_to_postgres/saver.py
@@ -1,9 +1,8 @@
 # sqlite_to_postgres/saver.py
-# пишет данные в PostgreSQL
+# пишет данные в PostgreSQL пачками
 from psycopg import Connection
-from typing import List
+from typing import Iterable
 from dataclasses import asdict
-
 
 class PostgresSaver:
     def __init__(self, conn: Connection):
@@ -11,18 +10,21 @@ class PostgresSaver:
 
     def save_all(self, data: dict):
         with self.conn.cursor() as cur:
-            for table, rows in data.items():
-                if not rows:
-                    continue
-                columns = list(asdict(rows[0]).keys())
-                values = [tuple(asdict(row).values()) for row in rows]
+            for table, rows_iterator in data.items():
+                for chunk in rows_iterator:
+                    if not chunk:
+                        continue
 
-                placeholders = ', '.join(['%s'] * len(columns))
-                column_list = ', '.join(columns)
-                query = f"""
-                    INSERT INTO content.{table} ({column_list})
-                    VALUES ({placeholders})
-                    ON CONFLICT (id) DO NOTHING
-                """
-                cur.executemany(query, values)
+                    columns = list(asdict(chunk[0]).keys())
+                    values = [tuple(asdict(row).values()) for row in chunk]
+
+                    placeholders = ', '.join(['%s'] * len(columns))
+                    column_list = ', '.join(columns)
+                    query = f"""
+                        INSERT INTO content.{table} ({column_list})
+                        VALUES ({placeholders})
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.executemany(query, values)
+
         self.conn.commit()
